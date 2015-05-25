95aa17b3ee0dcc8f6226c64514c4b463edc90472
EK <elmahdi.korfed@gmail.com>
2015-05-25 15:20:35 +0200

    implementation et personnalisation du calendrier d'activites

--- target/smartwatchproject-1.0-SNAPSHOT/styles/js/func.js
+++ target/smartwatchproject-1.0-SNAPSHOT/styles/js/func.js
@@ -19,11 +19,79 @@ $(function(){
 	
 	viewMap();
 
+
+
+                $('#calendar').fullCalendar({
+                    lang: 'fr',
+                    header: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'month,basicWeek,basicDay'
+                    },
+                    editable: true,
+                    eventLimit: true, // allow "more" link when too many events
+                    events: [
+                        {
+                            title: 'All Day Event',
+                            start: '2015-05-01'
+                        },
+                        {
+                            title: 'Long Event',
+                            start: '2015-05-07',
+                            end: '2015-05-10'
+                        },
+                        {
+                            id: 999,
+                            title: 'Repeating Event',
+                            start: '2015-05-09T16:00:00'
+                        },
+                        {
+                            id: 999,
+                            title: 'Repeating Event',
+                            start: '2015-05-16T16:00:00'
+                        },
+                        {
+                            title: 'Conference',
+                            start: '2015-05-11',
+                            end: '2015-05-13'
+                        },
+                        {
+                            title: 'Meeting',
+                            start: '2015-05-12T10:30:00',
+                            end: '2015-05-12T12:30:00'
+                        },
+                        {
+                            title: 'Lunch',
+                            start: '2015-05-12T12:00:00'
+                        },
+                        {
+                            title: 'Meeting',
+                            start: '2015-05-12T14:30:00'
+                        },
+                        {
+                            title: 'Happy Hour',
+                            start: '2015-05-12T17:30:00'
+                        },
+                        {
+                            title: 'Dinner',
+                            start: '2015-05-12T20:00:00'
+                        },
+                        {
+                            title: 'Birthday Party',
+                            start: '2015-05-13T07:00:00'
+                        },
+                        {
+                            title: 'Click for Google',
+                            url: 'http://google.com/',
+                            start: '2015-05-28'
+                        }
+                    ]
+                });
 })
 
 function chgBG(){
 	if (_numIMG>3) _numIMG=1;
-	console.log(_numIMG);
+	//console.log(_numIMG);
 	if (_numIMG==1){
 		chgMSG("Votre programme d'entrainement individuel sur votre poignet.","Parfaitement adapté à vos objectifs.");
 	} else if (_numIMG==2) {
